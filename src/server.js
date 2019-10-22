699e9d062d28d0f7cb59adaa6e22d167fe7f0c36
Lucas Borges <lborges801@gmail.com>
2019-10-22 14:58:17 -0300

    Estrutura backend

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors')
+
+
+const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+
+const url = 'mongodb+srv://omnistack:omnistack@cluster0-qkf5f.mongodb.net/test?retryWrites=true&w=majority';
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true,  useUnifiedTopology: true};
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box =>{
+        socket.join(box);
+    })
+})
+
+mongoose.connect(url, options)
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+})
+
+app.use(require('./routes'))
+
+server.listen(8080);
+
